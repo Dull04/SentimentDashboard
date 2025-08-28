c91818b8e5c28968bbd3cd6cf0d764b73f984b90
Dull04 <abdulazizfebriansyah@gmail.com>
2025-08-28 23:47:53 +0700

    (First Commit) setup project vue + install dependencies + library

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="container-fluid bg-dark min-vh-100 d-flex align-items-center justify-content-center">
+    <h1 class="text-primary">Hello World with Bootstraasp 🚀</h1>
+  </div>
+</template>
